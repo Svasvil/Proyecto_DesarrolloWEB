25fef7b9e86b7d7ecac11e50d461bf5e77045c45
DavidSanabriaVindas <dsanabria30061@ufide.ac.cr>
2025-03-09 01:05:18 -0600

    Controller y domain reservas

--- ProyectoDesApliWeb_Grupo3/src/main/java/Proyecto/Proyecto/controller/ReservasController.java
+++ ProyectoDesApliWeb_Grupo3/src/main/java/Proyecto/Proyecto/controller/ReservasController.java
@@ -1,14 +1,29 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
 package Proyecto.Proyecto.controller;
 
-/**
- *
- * @author Hp
- */
-public class ReservasController {
+import Proyecto.Proyecto.domain.Reservas;
+import Proyecto.Proyecto.service.ReservasService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+@Controller
+@RequestMapping("/hotel")
+public class ReservasController {
+    
+    @Autowired
+    private ReservasService reservasService;
+
+    @GetMapping("/listado")
+    public String inicio(Model model) {
+        var reservas = reservasService.getReservas(false);
+        model.addAttribute("reservas", reservas);
+        model.addAttribute("totalReservas", reservas.size());
+        return "/reserva/listado";
+    }
+    
 }
--- ProyectoDesApliWeb_Grupo3/src/main/java/Proyecto/Proyecto/domain/Reservas.java
+++ ProyectoDesApliWeb_Grupo3/src/main/java/Proyecto/Proyecto/domain/Reservas.java
@@ -1,5 +1,43 @@
 package Proyecto.Proyecto.domain;
 
-public class Reservas {
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "reservas")
+
+public class Reserva implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "idReserva")
+    private Integer idReserva;
+    @Column(name = "idUsuario")
+    private Integer idUsuario;
+    @Column(name = "idHabitacion")
+    private Integer idHabitacion;
+    private boolean activa;
+    private String fecha;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "categoriaHabitacion")
+    private CategoriaHabitacion categoriaHabitacion;
+
+    public enum CategoriaHabitacion {
+        Suite, Estandar, Premium
+    }
+
+    public Reserva() {
+    }
+
+    public Reserva(Integer idUsuario, Integer idHabitacion, boolean activa, String fecha, CategoriaHabitacion categoriaHabitacion) {
+        this.idUsuario = idUsuario;
+        this.idHabitacion = idHabitacion;
+        this.activa = activa;
+        this.fecha = fecha;
+        this.categoriaHabitacion = categoriaHabitacion;
+    }
 }
